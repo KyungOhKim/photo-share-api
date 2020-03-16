a37cde823e19cb1a3fcea62eaa82679ea7976adc
kko0831 <kko0831@hanmail.net>
2020-03-16 17:20:31 +0900

    5.2.5 커스텀 스칼라

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { ApolloServer } = require("apollo-server");
+const { GraphQLScalarType } = require("graphql");
 
 const typeDefs = `
     enum PhotoCategory {
@@ -15,6 +16,7 @@ const typeDefs = `
       postedPhotos: [Photo!]!
       inPhotos: [Photo!]!
     }
+    scalar DateTime
     type Photo {
       id: ID!
       url: String!
@@ -23,6 +25,7 @@ const typeDefs = `
       category: PhotoCategory!
       postedBy: User!
       taggedUsers: [User!]!
+      created: DateTime!
     }
     input PostPhotoInput {
       name: String!
@@ -31,7 +34,7 @@ const typeDefs = `
     }
     type Query {
         totalPhotos: Int!
-        allPhotos: [Photo!]!
+        allPhotos(after: DateTime): [Photo!]!
     }
     type Mutation {
       postPhoto(input: PostPhotoInput!): Photo!
@@ -59,20 +62,23 @@ var photos = [
     name: "Dropping the Heart Chute",
     description: "The heart chute is one of my favorite chutes",
     category: "ACTION",
-    githubUser: "gPlake"
+    githubUser: "gPlake",
+    created: "3-28-1977"
   },
   {
     id: "2",
     name: "Enjoying the sunshine",
     category: "SELFIE",
-    githubUser: "sSchmidt"
+    githubUser: "sSchmidt",
+    created: "1-2-1985"
   },
   {
     id: "3",
     name: "Gunbarrel 25",
     description: "25 laps on gunbarrel today",
     category: "LANDSCAPE",
-    githubUser: "sSchmidt"
+    githubUser: "sSchmidt",
+    created: "2018-04-15T19:09:57.308Z"
   }
 ];
 var tags = [
@@ -91,7 +97,8 @@ const resolvers = {
     postPhoto(parent, args) {
       var newPhoto = {
         id: _id++,
-        ...args.input
+        ...args.input,
+        created: new Date()
       };
       photos.push(newPhoto);
       return newPhoto;
@@ -117,7 +124,14 @@ const resolvers = {
         .filter(tag => tag.userID === parent.id)
         .map(tag => tag.photoID)
         .map(photoID => photos.find(p => p.id === photoID))
-  }
+  },
+  DateTime: new GraphQLScalarType({
+    name: "DateTime",
+    description: "A valid date time value.",
+    parseValue: value => new Date(value),
+    serialize: value => new Date(value).toISOString(),
+    parseLiteral: ast => ast.value
+  })
 };
 
 const server = new ApolloServer({
