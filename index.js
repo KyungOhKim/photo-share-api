42f984db8470cb0a019aa33911c7833c172ade7b
kko0831 <kko0831@hanmail.net>
2020-03-18 17:43:09 +0900

    5.4.2 컨텍스트에 데이터베이스 추가하기

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 const { ApolloServer } = require("apollo-server-express");
 const express = require("express");
 const expressPlayground = require("graphql-playground-middleware-express")
@@ -7,19 +9,27 @@ const { readFileSync } = require("fs");
 const typeDefs = readFileSync("./typeDefs.graphql", "UTF-8");
 const resolvers = require("./resolvers");
 
-var app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-});
+async function start() {
+  const app = express();
+  const MONGO_DB = process.env.DB_HOST;
 
-server.applyMiddleware({ app });
+  const client = await MongoClient.connect(MONGO_DB, { useNewUrlParser: true });
+  const db = client.db();
+  const context = { db };
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context
+  });
 
-app.get("/", (req, res) => res.end("Welcome to the PhotoShare API"));
-app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
+  server.applyMiddleware({ app });
+  app.get("/", (req, res) => res.end("Welcome to the PhotoShare API"));
+  app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
+  app.listen({ port: 4000 }, () =>
+    console.log(
+      `GraphQL Server running at http://localhost:4000${server.graphqlPath}`
+    )
+  );
+}
 
-app.listen({ port: 4000 }, () =>
-  console.log(
-    `GraphQL Server running http://localhost:4000${server.graphqlPath}`
-  )
-);
+start();
